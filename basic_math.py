98586e6303a67ef65203ec0bf8c3b1ac056bbc9c
hiab gebremichael <hiab.gebremichael.stu.ocsb.ca>
2025-02-13 18:02:17 +0000

    basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,10 @@
+# #!/usr/bin/env python 3 
+# # Created by: Hiab G 
+# # Date: feb. 13, 2025 
+# # This program does basic math.
+print(2+5)
+print(10-5)
+print(5*5)
+print(7/3)
+print(5 ^ 5)
+print(3 > 9)
